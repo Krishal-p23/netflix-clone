bdeb69043b50add5aa5004fa890b11d9edecd674
Krishal <aruhhh23@gmail.com>
2026-06-29 01:35:02 +0530

    Add user schema

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import { model, models, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    favorites: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Movie"
+        }
+    ]
+}, { timestamps: true });
+
+userSchema.pre("save", async function () {
+    if (this.password && this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+});
+
+const User = models?.User || model("User", userSchema);
+
+export default User;
