d2849bd7f3932b8294a93c455261937b0c34a59a
Krishal <aruhhh23@gmail.com>
2026-06-28 15:19:18 +0530

    Basic setup

---
+++ assets/assets.js
@@ -0,0 +1,41 @@
+import cards_data from "./cards/Cards_data";
+
+import back_arrow_icon from "./back_arrow_icon.png";
+import background_banner from "./background_banner.jpg";
+import bell_icon from "./bell_icon.svg";
+import caret_icon from "./caret_icon.svg";
+import facebook_icon from "./facebook_icon.png";
+import hero_banner from "./hero_banner.jpg";
+import hero_title from "./hero_title.png";
+import info_icon from "./info_icon.png";
+import instagram_icon from "./instagram_icon.png";
+import logo from "./logo.png";
+import netflix_icon from "./netflix_favicon.ico";
+import netflix_spinner from "./netflix_spinner.gif";
+import play_icon from "./play_icon.png";
+import profile_image from "./profile_img.png";
+import search_icon from "./search_icon.svg";
+import twitter_icon from "./twitter_icon.png";
+import youtube_icon from "./youtube_icon.png";
+
+export const assets = {
+    netflix_icon,
+    back_arrow_icon,
+    background_banner,
+    bell_icon,
+    caret_icon,
+    facebook_icon,
+    hero_banner,
+    hero_title,
+    info_icon,
+    instagram_icon,
+    logo,
+    netflix_spinner,
+    play_icon,
+    profile_image,
+    search_icon,
+    twitter_icon,
+    youtube_icon,
+
+    cards_data,
+}
